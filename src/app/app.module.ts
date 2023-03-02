ea07d42354fdf3875f276d692e62d23c4e2814bb
QuentinGilleron <98163779+QuentinGilleron@users.noreply.github.com>
2023-03-02 15:53:47 +0100

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { ShippingComponent } from './shipping/shipping.component';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   declarations: [
@@ -31,7 +32,7 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
-    ShippingComponent,
+    ShippingComponent
   ],
   bootstrap: [
     AppComponent
